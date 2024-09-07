8d073aee9297314c65cb0ffdfb6ab44bc10ddc22
Dharmik Vaghasana <vaghasana.dharmik@einfochips.com>
2024-09-07 11:52:48 +0530

    after tag v1.0.0.

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main() {
 	printf("My Hello world program\n");
 	printf("My Hello world program with Modified commit\n");
 	printf("My hello program with lates modification after tagged\n");
+	printf("My hello program with latest modification after tagged v1.0.0.\n");
 	return 0;
 }
